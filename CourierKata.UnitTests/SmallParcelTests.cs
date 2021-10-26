c456abf66368896ea00b49b8c326e14ef25dc28e
Foysal Ahmed <foysal.ahmed@mmtdigital.co.uk>
2021-10-26 13:05:19 +0100

    CourierKata.UnitTests SmallParcelTests fixture created with BasicSmallParcelTest test base implementation

---
+++ CourierKata.UnitTests/SmallParcelTests.cs
@@ -0,0 +1,53 @@
+﻿using Moq;
+using NUnit.Framework;
+
+using CourierKata.Library.Helpers;
+using CourierKata.Library.Interfaces;
+using CourierKata.Models;
+
+namespace CourierKata.UnitTests
+{
+    [TestFixture]
+    public class SmallParcelTests
+    {
+        private MockRepository _mockRepository;
+
+        private IParcelHelper _parcelHelper;
+
+        [SetUp]
+        public void Setup()
+        {
+            /* Moq setup */
+            _mockRepository = new MockRepository(MockBehavior.Strict);
+
+            /* Interface setup */
+            _parcelHelper = new ParcelHelper();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _mockRepository.VerifyAll();
+        }
+
+        [Test]
+        public void BasicSmallParcelTest()
+        {
+            /* Arrange */
+            var parcelDimensions = new ParcelDimensions
+            {
+                Length = 1,
+                Width = 1,
+                Height = 1
+            };
+            var expectedResult = 10;
+
+            /* Act */
+            var actualResult = _parcelHelper.GetDeliveryCost(parcelDimensions);
+
+            /* Assert */
+            Assert.IsNotNull(actualResult);
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+    }
+}
