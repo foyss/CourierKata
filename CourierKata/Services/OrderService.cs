858d4d9983242cab90d9a5889a97a2d872be3e10
Foysal Ahmed <foysal.ahmed@mmtdigital.co.uk>
2021-10-26 18:27:54 +0100

    CourierKata.Library OrderService base implementation to handle parcel dimensions and calculate total cost

---
+++ CourierKata/Services/OrderService.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using CourierKata.Library.Helpers;
+using CourierKata.Library.Interfaces;
+using CourierKata.Models;
+
+namespace CourierKata.Library.Services
+{
+    public class OrderService
+    {
+        private readonly IParcelHelper _parcelHelper;
+        private readonly IShippingHelper _shippingHelper;
+
+        public OrderService()
+        {
+            // These could be injected in via DI should the format of the project be different
+            _parcelHelper = new ParcelHelper(); 
+            _shippingHelper = new ShippingHelper();
+        }
+        public OrderDetails CreateOrder(List<ParcelDimensions> parcelDimensionsList, bool speedyDelivery)
+        {
+            var parcelDetailsList = _parcelHelper.GetParcelDetails(parcelDimensionsList);
+            var parcelDetailsCostSum = parcelDetailsList.Sum(t => t.Cost);
+
+            if (speedyDelivery)
+                parcelDetailsCostSum = _shippingHelper.SpeedyShipping(parcelDetailsCostSum);
+
+            return new OrderDetails
+            {
+                Parcels = parcelDetailsList,
+                TotalCost = parcelDetailsCostSum
+            };
+        }
+    }
+}
