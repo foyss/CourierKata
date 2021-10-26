99eb690f17234312bd961f876312e55747be8108
Foysal Ahmed <foysal.ahmed@mmtdigital.co.uk>
2021-10-26 18:37:05 +0100

    CourierKata.UnitTests OrderServiceUnitTests created CreateOrderTests test

--- CourierKata.UnitTests/OrderServiceUnitTests.cs
+++ CourierKata.UnitTests/OrderServiceUnitTests.cs
@@ -1,8 +1,12 @@
-﻿using Moq;
+﻿using System.Collections.Generic;
+using Moq;
 using NUnit.Framework;
 
 using CourierKata.Library.Interfaces;
 using CourierKata.Library.Services;
+using CourierKata.Models;
+using Microsoft.VisualStudio.TestPlatform.CommunicationUtilities;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel;
 
 namespace CourierKata.UnitTests
 {
@@ -26,6 +30,28 @@ namespace CourierKata.UnitTests
         public void TearDown()
         {
             _mockRepository.VerifyAll();
+        }
+
+        [TestCase(1, 1, 1, true)]
+        public void CreateOrderTests(decimal length, decimal width, decimal height, bool speedyShipping)
+        {
+            /* Arrange */
+            var listOfParcels = new List<ParcelDimensions>
+            {
+                new()
+                {
+                    Length = length,
+                    Width = width,
+                    Height = height
+                }
+            };
+
+            /* Act */
+            var actualResult = _orderService.CreateOrder(listOfParcels, speedyShipping);
+
+            /* Assert */
+            Assert.IsNotNull(actualResult);
+            Assert.IsInstanceOf<OrderDetails>(actualResult);
         }
     }
 }
