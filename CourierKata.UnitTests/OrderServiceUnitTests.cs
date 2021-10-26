aa7233090aa7f668b9d88bb0dc11c896c36405ed
Foysal Ahmed <foysal.ahmed@mmtdigital.co.uk>
2021-10-26 18:30:37 +0100

    CourierKata.UnitTests OrderServiceUnitTests created

---
+++ CourierKata.UnitTests/OrderServiceUnitTests.cs
@@ -0,0 +1,31 @@
+﻿using Moq;
+using NUnit.Framework;
+
+using CourierKata.Library.Interfaces;
+using CourierKata.Library.Services;
+
+namespace CourierKata.UnitTests
+{
+    public class OrderServiceUnitTests
+    {
+        private MockRepository _mockRepository;
+
+        private IOrderService _orderService;
+
+        [SetUp]
+        public void Setup()
+        {
+            /* Moq setup */
+            _mockRepository = new MockRepository(MockBehavior.Strict);
+
+            /* Interface setup */
+            _orderService = new OrderService();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _mockRepository.VerifyAll();
+        }
+    }
+}
