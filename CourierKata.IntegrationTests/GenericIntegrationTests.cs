e111a6b5d8ce40345e5f4cc3fe3f4c5e71cc36c1
Foysal Ahmed <foysal.ahmed@mmtdigital.co.uk>
2021-10-26 19:18:50 +0100

    GenericIntegrationTests created as an input method to test library and referenced projects

---
+++ CourierKata.IntegrationTests/GenericIntegrationTests.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+
+namespace CourierKata.IntegrationTests
+{
+    /// <summary>
+    /// NOTE: This test class will be used as an input in order to test the project
+    /// </summary>
+    [TestFixture]
+    public class GenericIntegrationTests
+    {
+
+        [SetUp]
+        public void Setup()
+        {
+            /* Interface setup */
+        }
+
+        /// <summary>
+        /// NOTE: This test will be used as an input in order to test the project
+        /// </summary>
+        [Test]
+        public void ConsolePlayground()
+        {
+        }
+    }
+}
