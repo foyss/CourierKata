6210e1d598b95034b61b1af0b3e7c49b132bf579
Foysal Ahmed <foysal.ahmed@mmtdigital.co.uk>
2021-10-26 13:03:10 +0100

    CourierKata.Library created Helpers folder, moved ParcelHelper

---
+++ CourierKata/Helpers/ParcelHelper.cs
@@ -0,0 +1,18 @@
+﻿using CourierKata.Library.Interfaces;
+using CourierKata.Models;
+
+namespace CourierKata.Library.Helpers
+{
+    public class ParcelHelper : IParcelHelper
+    {
+        public ParcelHelper()
+        {
+
+        }
+
+        public decimal GetDeliveryCost(ParcelDimensions parcelDimensions)
+        {
+            return default;
+        }
+    }
+}
