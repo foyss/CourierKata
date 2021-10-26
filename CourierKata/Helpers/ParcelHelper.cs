fcd8d0f4a75fe19e700ae766ce91c4bd98eb1b38
Foysal Ahmed <foysal.ahmed@mmtdigital.co.uk>
2021-10-26 19:16:07 +0100

    CourierKata.Library ParcelHelper updated GetParcelDetails to handle heavy weight

--- CourierKata/Helpers/ParcelHelper.cs
+++ CourierKata/Helpers/ParcelHelper.cs
@@ -13,59 +13,67 @@ namespace CourierKata.Library.Helpers
             foreach (var t in parcels)
             {
                 decimal totalDimensionCount = t.Height + t.Length + t.Width;
-                decimal parcelDeliveryCost;
+                decimal parcelDeliveryCost = 0;
+                bool heavyParcel = t.Weight >= 51;
+
+                if (heavyParcel)
+                    parcelDeliveryCost += CalculateOverWeightCost(t.Weight, ParcelWeightEnum.Heavy);
 
                 switch(totalDimensionCount)
                 {
                     case < 10:
-                        parcelDeliveryCost = 3;
+                        parcelDeliveryCost += 3;
 
-                        if (t.Weight >= 2)
-                            parcelDeliveryCost += CalculateOverWeightCost(t.Weight, ParcelSizeEnum.Small);
+                        if (t.Weight >= 2 && !heavyParcel)
+                            parcelDeliveryCost += CalculateOverWeightCost(t.Weight, ParcelWeightEnum.Small);
 
                         break;
                     case < 50:
-                        parcelDeliveryCost = 8;
+                        parcelDeliveryCost += 8;
 
-                        if (t.Weight >= 4)
-                            parcelDeliveryCost += CalculateOverWeightCost(t.Weight, ParcelSizeEnum.Medium);
+                        if (t.Weight >= 4 && !heavyParcel)
+                            parcelDeliveryCost += CalculateOverWeightCost(t.Weight, ParcelWeightEnum.Medium);
 
                         break;
 
                     case < 100:
-                        parcelDeliveryCost = 15;
+                        parcelDeliveryCost += 15;
 
-                        if (t.Weight >= 7)
-                            parcelDeliveryCost += CalculateOverWeightCost(t.Weight, ParcelSizeEnum.Large);
+                        if (t.Weight >= 7 && !heavyParcel)
+                            parcelDeliveryCost += CalculateOverWeightCost(t.Weight, ParcelWeightEnum.Large);
 
                         break;
                     case >= 100:
-                        parcelDeliveryCost = 25;
+                        parcelDeliveryCost += 25;
 
-                        if (t.Weight >= 11)
-                            parcelDeliveryCost += CalculateOverWeightCost(t.Weight, ParcelSizeEnum.XL);
+                        if (t.Weight >= 11 && !heavyParcel)
+                            parcelDeliveryCost += CalculateOverWeightCost(t.Weight, ParcelWeightEnum.XL);
 
                         break;;
-                };
+                }
+
                 list.Add(new ParcelDetails {Length = t.Length, Width = t.Width, Height = t.Height, Cost = parcelDeliveryCost});
             }
 
             return list;
         }
 
-        private decimal CalculateOverWeightCost(decimal weight, ParcelSizeEnum parcelSize)
+        private decimal CalculateOverWeightCost(decimal weight, ParcelWeightEnum parcelWeight)
         {
             var overWeightCharge = 2;
-            var weightLimit = parcelSize switch
+            var weightLimit = parcelWeight switch
             {
-                ParcelSizeEnum.Small => 1,
-                ParcelSizeEnum.Medium => 3,
-                ParcelSizeEnum.Large => 6,
-                ParcelSizeEnum.XL => 10,
+                ParcelWeightEnum.Small => 1,
+                ParcelWeightEnum.Medium => 3,
+                ParcelWeightEnum.Large => 6,
+                ParcelWeightEnum.XL => 10,
+                ParcelWeightEnum.Heavy => 50,
                 _ => 0
             };
 
-            return (weight - weightLimit) * overWeightCharge;
+            return parcelWeight == ParcelWeightEnum.Heavy
+                ? (weight - weightLimit) + 50
+                : (weight - weightLimit) * overWeightCharge;
         }
     }
 }
