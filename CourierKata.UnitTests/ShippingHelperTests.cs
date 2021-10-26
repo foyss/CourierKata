69973e3e5642e5d1dd50aee672fff59d6bfcfe27
Foysal Ahmed <foysal.ahmed@mmtdigital.co.uk>
2021-10-26 18:19:10 +0100

    CourierKata.UnitTests ShippingHelperTests created

---
+++ CourierKata.UnitTests/ShippingHelperTests.cs
@@ -0,0 +1,32 @@
+﻿using Moq;
+using NUnit.Framework;
+
+using CourierKata.Library.Helpers;
+using CourierKata.Library.Interfaces;
+
+namespace CourierKata.UnitTests
+{
+    [TestFixture]
+    public class ShippingHelperTests
+    {
+        private IShippingHelper _shippingHelper;
+
+        private MockRepository _mockRepository;
+
+        [SetUp]
+        public void Setup()
+        {
+            /* Moq setup */
+            _mockRepository = new MockRepository(MockBehavior.Strict);
+
+            /* Interface setup */
+            _shippingHelper = new ShippingHelper();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _mockRepository.VerifyAll();
+        }
+    }
+}
