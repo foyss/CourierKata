eb827137527a359e4d0f8c986fb4c00a5b9c04a4
Foysal Ahmed <foysal.ahmed@mmtdigital.co.uk>
2021-10-26 18:28:03 +0100

    CourierKata.Models OrderDetails created

---
+++ CourierKata.Models/OrderDetails.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace CourierKata.Models
+{
+    public class OrderDetails
+    {
+        public List<ParcelDetails> Parcels { get; set; }
+        public decimal TotalCost { get; set; }
+    }
+}
